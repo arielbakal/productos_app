5b4bad2f7c12a6879b6dd5046aa0c91c8130cdfd
arielbakal <arielbakal.dev@gmail.com>
2022-06-25 20:45:09 -0300

    225, database created & https petition test

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/services/services.dart';
 import 'package:productos_app/widgets/product_card.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
-   
-  const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
+
+    final productsService = Provider.of<ProductsService>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Productos')
