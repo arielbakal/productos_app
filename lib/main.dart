e32decaf43e752206d4d6d76b357c30ae7e9d197
arielbakal <arielbakal.dev@gmail.com>
2022-06-25 18:48:20 -0300

    222

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,9 @@ class MyApp extends StatelessWidget {
       title: 'Productos App',
       initialRoute: 'home',
       routes: {
-        'login': ( _ ) => const LoginScreen(),
-        'home': ( _ ) => const HomeScreen(),
+        'login'   : ( _ ) => LoginScreen(),
+        'home'    : ( _ ) => HomeScreen(),
+        'product' : ( _ ) => ProductScreen(),
       },
       theme: ThemeData.light().copyWith(
         scaffoldBackgroundColor: Colors.grey[300],
