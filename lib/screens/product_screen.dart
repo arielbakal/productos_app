a364846a0b2eba35f977847a2f1cbe45117e90af
arielbakal <arielbakal.dev@gmail.com>
2022-06-29 13:26:19 -0300

    239

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -71,7 +71,6 @@ class _ProductScreenBody extends StatelessWidget {
                       return;
                     }
 
-                    print('Tenemos imagen ${pickedFile.path}');
                     productService.updateSelectedProductImage(pickedFile.path);
 
                     },
@@ -95,6 +94,10 @@ class _ProductScreenBody extends StatelessWidget {
         onPressed: () async {
 
           if ( !productForm.isValidForm() ) return;
+
+          final String? imageUrl = await productService.uploadImage();
+
+          print( imageUrl );
 
           await productService.saveOrCreateProduct(productForm.product);
         },
