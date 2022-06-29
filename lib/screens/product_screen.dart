fe77b70e9c8fedc7a6cbac12380ce845f3127e44
arielbakal <arielbakal.dev@gmail.com>
2022-06-29 11:41:37 -0300

    237

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:productos_app/providers/product_form_provider.dart';
 import 'package:productos_app/services/services.dart';
 import 'package:productos_app/ui/input_decorations.dart';
@@ -57,7 +58,22 @@ class _ProductScreenBody extends StatelessWidget {
                   top: 60,
                   right: 20,
                   child: IconButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                    
+                    final ImagePicker picker = ImagePicker();
+                    final XFile? pickedFile = await picker.pickImage(
+                      source: ImageSource.camera,
+                      imageQuality: 100
+                    );
+
+                    if ( pickedFile == null ) {
+                      print('No selecciono nada');
+                      return;
+                    }
+
+                    print('Tenemos imagen ${pickedFile.path}');
+
+                    },
                     icon: Icon( Icons.camera_alt_outlined, size: 40, color: Colors.white )
                   )
                 ),
